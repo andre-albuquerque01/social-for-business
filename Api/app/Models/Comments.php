5b91525f0c3460de4ce2d8ea630db054c7a152a9
Andre <andrealbuquerque2001@gmail.com>
2024-04-24 15:08:32 -0300

    feat: create new models

---
+++ Api/app/Models/Comments.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Comments extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'comment',
+    ];
+
+    public function BelongUser()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function BelongPost()
+    {
+        return $this->belongsTo(Posts::class);
+    }
+}
---
+++ Api/app/Models/Rate.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Rate extends Model
+{
+    use HasFactory, HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'qtdRate',
+        'isRating',
+    ];
+
+    public function BelongUser()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function BelongPost()
+    {
+        return $this->belongsTo(Posts::class);
+    }
+}
