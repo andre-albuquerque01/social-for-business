2f09222476b4085473316399c95d909f89faa811
Andre <andrealbuquerque2001@gmail.com>
2024-04-25 16:29:24 -0300

    feat: update in route

--- Api/routes/api.php
+++ Api/routes/api.php
@@ -1,12 +1,17 @@
 <?php
 
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::post('login', [AuthController::class, 'login']);
 
-Route::get("/", function (){
-    return response()->json(['saasdad' => 'Brasil']);
+    Route::prefix('user')->group(function () {
+        Route::post('store', [UserController::class, 'store']);
+        Route::get('show', [UserController::class, 'show']);
+        Route::put('update', [UserController::class, 'update']);
+        Route::get('', [UserController::class, 'delete']);
+    });
 });
