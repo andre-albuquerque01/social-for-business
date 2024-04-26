4eee9ec28774e5a4d90fb856909dada06a9c140f
Andre <andrealbuquerque2001@gmail.com>
2024-04-26 15:37:44 -0300

    feat: improvement in model

--- Api/app/Models/Posts.php
+++ Api/app/Models/Posts.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Posts extends Model
 {
@@ -19,19 +21,19 @@ class Posts extends Model
         'imageUrlTwo',
         'imageUrlThree',
         'description',
-        'user_id',
+        // 'user_idUser',
     ];
 
-    public function user()
+    public function user(): BelongsToMany
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsToMany(User::class);
     }
 
-    public function comments()
+    public function comments(): HasMany
     {
         return $this->hasMany(Comments::class);
     }
-    public function rates()
+    public function rates(): HasMany
     {
         return $this->hasMany(Rate::class);
     }
