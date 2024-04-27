dffb1e04008006c873cf434c453d976b80a7df32
Andre <andrealbuquerque2001@gmail.com>
2024-04-27 19:12:50 -0300

    feat: create new comment controller

---
+++ Api/app/Http/Controllers/Api/CommentController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CommentRequest;
+use App\Services\CommentService;
+use Illuminate\Http\Request;
+
+class CommentController extends Controller
+{
+
+    private $commentService;
+    public function __construct(CommentService $commentService)
+    {
+        $this->commentService = $commentService;
+    }
+
+    public function show(string $id)
+    {
+        return $this->commentService->show($id);
+    }
+
+    public function store(CommentRequest $request)
+    {
+        dd($request->validated());
+        // return $this->commentService->store($request->validated());
+    }
+
+    public function update(CommentRequest $data, string $id)
+    {
+        return $this->commentService->update($data->validated(), $id);
+    }
+
+    public function destroy(string $id)
+    {
+        return $this->commentService->destroy($id);
+    }
+}
