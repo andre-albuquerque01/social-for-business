b7d174972eb234b5ecab62dcd33aea40c6389f84
Andre <andrealbuquerque2001@gmail.com>
2024-04-27 19:12:41 -0300

    feat: create new comment exception

---
+++ Api/app/Exceptions/CommentException.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class CommentException extends Exception
+{
+    protected $message = 'Error while creating comment';
+    public function render()
+    {
+        return response()->json([
+            'error' => class_basename($this),,
+            'message' => $this->message,
+        ], 401);
+    }
+}
