f805020bd3c68fd54e1a561aa321c4250390e4ee
Andre <andrealbuquerque2001@gmail.com>
2024-04-29 13:11:09 -0300

    feat: update in service

--- Api/app/Services/PostService.php
+++ Api/app/Services/PostService.php
@@ -3,69 +3,89 @@
 namespace App\Services;
 
 use App\Exceptions\PostException;
+use App\Http\Resources\PostResource;
+use App\Http\Resources\PostResource2;
 use App\Models\Posts;
 use Exception;
 use Illuminate\Support\Facades\Storage;
 
 class PostService
 {
-    public function store(array $data): string
+    public function store(array $data)
     {
         try {
-            $image = $data['imageUrlOne'];
-            $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
-            Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-            $data['imageUrlOne'] = $newName_image;
-
+            if ($data['imageUrlOne']) {
+                $image = $data['imageUrlOne'];
+                $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
+                Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
+                $data['imageUrlOne'] = $newName_image;
+            }
             if (isset($data['imageUrlTwo'])) {
                 $image = $data['imageUrlTwo'];
                 $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
                 Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlOne'] = $newName_image;
+                $data['imageUrlTwo'] = $newName_image;
             }
             if (isset($data['imageUrlThree'])) {
                 $image = $data['imageUrlThree'];
                 $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
                 Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlOne'] = $newName_image;
+                $data['imageUrlThree'] = $newName_image;
             }
-            
+
             auth()->user()->posts()->create($data);
 
-            return response()->json(['message' => 'success'], 200);
+            return new PostResource(['message' => 'success']);
         } catch (Exception $th) {
             throw new PostException('Error creating post');
         }
     }
 
-    public function update(array $data, $idPost): string
+    public function showUser()
     {
         try {
-            $post = Posts::findOrFail($idPost);
+            $show = auth()->user()->posts();
+            return new PostResource2($show->get());
+        } catch (Exception $th) {
+            throw new PostException('Error creating post');
+        }
+    }
 
-            $image = $data['imageUrlOne'];
-            $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
-            Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-            $data['imageUrlOne'] = $newName_image;
-
+    public function update(array $data, string $idPost)
+    {
+        try {    
+            if (isset($data['imageUrlOne'])) {
+                $image = $data['imageUrlOne'];
+                $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
+                Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
+                $data['imageUrlOne'] = $newName_image;
+            }
             if (isset($data['imageUrlTwo'])) {
                 $image = $data['imageUrlTwo'];
                 $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
                 Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlOne'] = $newName_image;
+                $data['imageUrlTwo'] = $newName_image;
             }
             if (isset($data['imageUrlThree'])) {
                 $image = $data['imageUrlThree'];
                 $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
                 Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlOne'] = $newName_image;
+                $data['imageUrlThree'] = $newName_image;
+            }
+
+            $user = auth()->user()->idUser;
+            
+            $post = Posts::where('idPost', $idPost)->where('user_idUser', $user)->first();
+            
+            if (!$post) {
+                return new PostResource(["message" => "Not found."]);
             }
             
             $post->update($data);
-            
-            return response()->json(['message' => 'success'], 200);
+
+            return new PostResource(['message' => 'success']);
         } catch (Exception $th) {
-            throw new PostException('Error creating post');
+            throw new PostException('Error update post');
         }
     }
 }
