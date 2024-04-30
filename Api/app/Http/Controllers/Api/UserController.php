cba21b2a606850af72981fda6b387d838e7f4ba7
Andre <andrealbuquerque2001@gmail.com>
2024-04-30 17:02:37 -0300

    feat: update in controller

--- Api/app/Http/Controllers/Api/UserController.php
+++ Api/app/Http/Controllers/Api/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RecoverPasswordRequest;
 use App\Http\Requests\UserStoreRequest;
 use App\Http\Resources\UserResource;
 use App\Http\Resources\UserResource2;
@@ -50,5 +51,25 @@ class UserController extends Controller
             'email' => 'required|email',
         ]);
         return $this->userService->reSendEmail($input);
+    }
+
+    public function recoverEmail(Request $data)
+    {
+        $input = $data->validate([
+            'email' => 'required|email',
+        ]);
+        return $this->userService->recoverEmail($input['email']);
+    }
+    public function verifyToken(Request $data)
+    {
+        $input = $data->validate([
+            'token' => 'required|min:7|max:9',
+        ]);
+        return $this->userService->verifyToken($input['token']);
+    }
+
+    public function recoverPassword(RecoverPasswordRequest $data)
+    {
+        return $this->userService->recoverPassword($data->validated());
     }
 }
