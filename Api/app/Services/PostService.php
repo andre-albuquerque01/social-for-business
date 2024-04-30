5c93b7666ced2de5a2daf32b2c23697cc19e0636
Andre <andrealbuquerque2001@gmail.com>
2024-04-30 11:01:05 -0300

    feat: update in service

--- Api/app/Services/PostService.php
+++ Api/app/Services/PostService.php
@@ -5,7 +5,6 @@ namespace App\Services;
 use App\Exceptions\PostException;
 use App\Http\Resources\GeneralResource;
 use App\Http\Resources\PostResource;
-use App\Http\Resources\PostResource2;
 use App\Models\Posts;
 use Exception;
 use Illuminate\Support\Facades\Storage;
@@ -15,8 +14,12 @@ class PostService
 
     public function index()
     {
-        $post = Posts::with('comments')->join('users','users.idUser','=','posts.user_idUser')->whereNull("posts.deleted_at")->get();
-        return PostResource::collection($post);
+        try {
+            $post = Posts::with('comments')->join('users', 'users.idUser', '=', 'posts.user_idUser')->whereNull("posts.deleted_at")->get();
+            return PostResource::collection($post);
+        } catch (Exception $th) {
+            throw new PostException('Error to list post');
+        }
     }
 
     public function store(array $data)
@@ -27,18 +30,6 @@ class PostService
                 $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
                 Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
                 $data['imageUrlOne'] = $newName_image;
-            }
-            if (isset($data['imageUrlTwo'])) {
-                $image = $data['imageUrlTwo'];
-                $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
-                Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlTwo'] = $newName_image;
-            }
-            if (isset($data['imageUrlThree'])) {
-                $image = $data['imageUrlThree'];
-                $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
-                Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlThree'] = $newName_image;
             }
 
             auth()->user()->posts()->create($data);
@@ -52,8 +43,8 @@ class PostService
     public function showUser()
     {
         try {
-            $show = auth()->user()->posts();
-            return new PostResource2($show->get());
+            $show = auth()->user()->posts()->with('comments');
+            return new PostResource($show->get());
         } catch (Exception $th) {
             throw new PostException('Error creating post');
         }
@@ -61,39 +52,45 @@ class PostService
 
     public function update(array $data, string $idPost)
     {
-        try {    
+        try {
             if (isset($data['imageUrlOne'])) {
                 $image = $data['imageUrlOne'];
                 $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
                 Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
                 $data['imageUrlOne'] = $newName_image;
             }
-            if (isset($data['imageUrlTwo'])) {
-                $image = $data['imageUrlTwo'];
-                $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
-                Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlTwo'] = $newName_image;
-            }
-            if (isset($data['imageUrlThree'])) {
-                $image = $data['imageUrlThree'];
-                $newName_image = date("H_i_s-d_m_Y.") . $image->getClientOriginalExtension();
-                Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                $data['imageUrlThree'] = $newName_image;
-            }
 
             $user = auth()->user()->idUser;
-            
+
             $post = Posts::where('idPost', $idPost)->where('user_idUser', $user)->first();
-            
+
             if (!$post) {
-                return new GeneralResource(["message" => "Not found."]);
+                return new GeneralResource(["message" => "Unathorized"]);
             }
-            
+
             $post->update($data);
 
             return new GeneralResource(['message' => 'success']);
         } catch (Exception $th) {
             throw new PostException('Error update post');
+        }
+    }
+
+    public function destroy(string $idPost)
+    {
+        try {
+            $user = auth()->user()->idUser;
+
+            $post = Posts::where('idPost', $idPost)->where('user_idUser', $user)->first();
+
+            if (!$post) {
+                return new GeneralResource(["message" => "Unathorized"]);
+            }
+
+            $post->touch("deleted_at");
+            return response()->json(['message' => 'success'], 200);
+        } catch (Exception $th) {
+            throw new PostException('Error destroy post');
         }
     }
 }
