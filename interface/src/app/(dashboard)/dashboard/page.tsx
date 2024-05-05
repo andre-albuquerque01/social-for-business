93455e44e37c89f0915f1d49877c43c9a1f97d68
Andre <andrealbuquerque2001@gmail.com>
2024-05-05 11:14:15 -0300

    feat: test pages

---
+++ interface/src/app/(dashboard)/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+  return (
+    <div className="h-screen bg-red-950 mx-auto text-center">
+      <p className="text-red">Brasil</p>
+    </div>
+  )
+}
--- interface/src/app/page.tsx
+++ interface/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="h-screen bg-zinc-950 mx-auto text-center">
-      aaaaaaaaaaaaaaa
+      <p className="text-white">aaaaaaaaaaaaaaa</p>
     </div>
   )
 }
