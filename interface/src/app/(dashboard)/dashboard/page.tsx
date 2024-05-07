d73fac89890f14494760824a20e19c0ddbd0442a
Andre <andrealbuquerque2001@gmail.com>
2024-05-07 16:43:28 -0300

    feat: update in page

--- interface/src/app/(dashboard)/dashboard/page.tsx
+++ interface/src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,18 @@
+import { CardPostsComponent } from '@/components/dashboard/cardPost'
+import { CardUserComponent } from '@/components/dashboard/cardUser'
+import { CreatePost } from '@/components/dashboard/createPost'
+
 export default function Dashboard() {
   return (
-    <div className="h-screen bg-red-950 mx-auto text-center">
-      <p className="text-red">Brasil</p>
+    <div className="min-h-screen text-white flex gap-4 p-4">
+      <CardUserComponent />
+      <div className="flex-1 space-y-4">
+        <CreatePost />
+        <CardPostsComponent />
+        <CardPostsComponent />
+        <CardPostsComponent />
+        <CardPostsComponent />
+      </div>
     </div>
   )
 }
