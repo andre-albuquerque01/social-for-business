e5bbbd4e3ca9b7b04ce0b0f01d8f1f56fcd614e3
Andre <andrealbuquerque2001@gmail.com>
2024-05-08 17:01:00 -0300

    feat: create new function

---
+++ interface/src/middleware.ts
@@ -0,0 +1,11 @@
+import { type NextRequest, NextResponse } from 'next/server'
+import verifyToken from './functions/verify-token'
+
+export async function middleware(request: NextRequest) {
+  const token = request.cookies.get('token')?.value
+  const authentication = token ? await verifyToken(token) : false
+  if (!authentication && request.nextUrl.pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/', request.url))
+  }
+  return NextResponse.next()
+}
