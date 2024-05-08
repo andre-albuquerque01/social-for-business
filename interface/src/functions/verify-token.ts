657ba9cf75e20b155de36064ce6b84ca47cb257c
Andre <andrealbuquerque2001@gmail.com>
2024-05-08 17:01:24 -0300

    feat: create new function

---
+++ interface/src/functions/verify-token.ts
@@ -0,0 +1,15 @@
+import { jwtVerify } from 'jose'
+
+export default async function verifyToken(token: string): Promise<boolean> {
+  if (!token) return false
+  try {
+    await jwtVerify(
+      token,
+      new TextEncoder().encode(process.env.NEXT_PRIVATE_KEY_JWT),
+      { algorithms: ['HS256'] },
+    )
+    return true
+  } catch (error) {
+    return false
+  }
+}
