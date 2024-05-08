d3a2a51a0e09a5d1f2b9a4bc15769b4b2bb991b1
Andre <andrealbuquerque2001@gmail.com>
2024-05-08 17:02:28 -0300

    feat: update component

--- interface/src/components/user/insert/insert.tsx
+++ interface/src/components/user/insert/insert.tsx
@@ -1,10 +1,38 @@
+'use client'
+import { InsertUser } from '@/actions/user/insert'
 import { ButtonComponent } from '@/components/form/button'
 import { InputComponent } from '@/components/form/input'
 import Link from 'next/link'
+import { useFormState, useFormStatus } from 'react-dom'
+
+function FormButton() {
+  const { pending } = useFormStatus()
+
+  return (
+    <>
+      {pending ? (
+        <button
+          className="bg-red-600 text-white px-4 py-2 w-96 max-md:w-80 max-md:mx-auto rounded-lg"
+          disabled={pending}
+        >
+          Enviando...
+        </button>
+      ) : (
+        <ButtonComponent title="Cadastar" />
+      )}
+    </>
+  )
+}
 
 export const InsertUserComponent = () => {
+  const [state, action] = useFormState(InsertUser, {
+    ok: false,
+    error: '',
+    data: null,
+  })
+
   return (
-    <form className="space-y-5 flex flex-col text-white">
+    <form className="space-y-5 flex flex-col text-white" action={action}>
       <Link href="/">{'<- Voltar'}</Link>
       <InputComponent
         type="text"
@@ -48,9 +76,11 @@ export const InsertUserComponent = () => {
           <Link href="" className="text-blue-600">
             termos de aceites
           </Link>
+          <span className="text-xs text-red-600"> *</span>
         </label>
       </div>
-      <ButtonComponent title="Cadastar" />
+      <p className="text-xs text-red-600">{state.error}</p>
+      <FormButton />
     </form>
   )
 }
