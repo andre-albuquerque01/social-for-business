14caf5f753fae59c9eb8256b9068709ec49b2632
Andre <andrealbuquerque2001@gmail.com>
2024-05-13 17:21:16 -0300

    feat: update action

--- interface/src/actions/post/create.ts
+++ interface/src/actions/post/create.ts
@@ -2,6 +2,7 @@
 
 import apiError from '@/functions/api-error'
 import ApiAction from '@/functions/data/apiAction'
+import { RevalidateTag } from '@/functions/revalidateTag'
 import { cookies } from 'next/headers'
 
 export async function CreatePostAction(
@@ -9,19 +10,26 @@ export async function CreatePostAction(
   request: FormData,
 ) {
   const description = request.get('description') as string | null
+  const imageUrlOne = request.get('imageUrlOne') as File | null
 
   try {
     if (!description) throw new Error('Preencha a descrição.')
+    if (imageUrlOne && imageUrlOne.size === 0) {
+      request.set('imageUrlOne', '')
+    }
 
     const response = await ApiAction('/post', {
       method: 'POST',
       headers: {
+        Accept: 'application/json',
         Authorization: 'Bearer ' + cookies().get('token')?.value,
       },
       body: request,
     })
 
     const data = await response.json()
+
+    RevalidateTag('post')
 
     if (
       data.message === 'The description field must be at least 10 characters.'
