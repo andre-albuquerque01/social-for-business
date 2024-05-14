3b984fd78d05dbf70f1af65e9ea6564a243d96ef
Andre <andrealbuquerque2001@gmail.com>
2024-05-14 17:13:52 -0300

    feat: create new action

---
+++ interface/src/actions/user/otherProfile.ts
@@ -0,0 +1,27 @@
+'use server'
+
+import apiError from '@/functions/api-error'
+import ApiAction from '@/functions/data/apiAction'
+import { cookies } from 'next/headers'
+
+export async function ShowOtherProfilePost(idUser: string) {
+  try {
+    const response = await ApiAction(`/post/showUser/${idUser}`, {
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer' + cookies().get('token')?.value,
+      },
+      next: {
+        revalidate: 60 * 30,
+        tags: ['post'],
+      },
+      // cache: 'no-cache',
+    })
+
+    const data = await response.json()
+
+    return data.data
+  } catch (err) {
+    return apiError(err)
+  }
+}
