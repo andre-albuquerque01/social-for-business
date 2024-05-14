ce525d33714bfa9444bdf4137e46e6889af473f5
Andre <andrealbuquerque2001@gmail.com>
2024-05-14 17:15:05 -0300

    feat: update component

--- interface/src/app/user/update/page.tsx
+++ interface/src/app/user/update/page.tsx
@@ -1,17 +1,20 @@
+import { ShowUser, UserInterface } from '@/actions/user/show'
 import UpdateUserComponent from '@/components/user/update/update'
 import { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: {
     template: '%s',
-    default: 'Insert user',
+    default: 'Alterar usuário',
   },
 }
 
-export default function UpdateUser() {
+export default async function UpdateUser() {
+  const data: UserInterface = await ShowUser()
+
   return (
     <div className="flex justify-center items-center h-screen">
-      <UpdateUserComponent />
+      <UpdateUserComponent data={data} />
     </div>
   )
 }
