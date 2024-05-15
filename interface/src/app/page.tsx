34b8cf8ca9223e3f3f74512eb5f7b924b071eaba
Andre <andrealbuquerque2001@gmail.com>
2024-05-15 12:11:42 -0300

    feat: update page

--- interface/src/app/page.tsx
+++ interface/src/app/page.tsx
@@ -1,7 +1,18 @@
+import { VerifyEmail } from '@/actions/user/verifyEmail'
 import { LoginComponent } from '@/components/user/login/login'
 import Image from 'next/image'
 
-export default function Home() {
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { q: string }
+}) {
+  let req: string = ''
+  if (searchParams.q) {
+    const { q } = searchParams
+    req = await VerifyEmail(q)
+  }
+
   return (
     <div className="h-screen max-w-[1200px] mx-auto flex gap-2 max-md:flex-col max-md:items-center max-md:justify-evenly">
       <div className="w-[48%] flex justify-center items-center max-sm:hidden">
@@ -14,7 +25,7 @@ export default function Home() {
         />
       </div>
       <div className="md:w-[48%] max-md:w-auto flex justify-center items-center">
-        <LoginComponent />
+        <LoginComponent message={req} />
       </div>
     </div>
   )
