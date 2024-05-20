9c0f52d3eb578881f76f99f0474dcc3b7b81612b
Andre <andrealbuquerque2001@gmail.com>
2024-05-20 16:59:07 -0300

    feat: update page

--- interface/src/app/user/profile/page.tsx
+++ interface/src/app/user/profile/page.tsx
@@ -2,8 +2,19 @@ import { ShowProfilePost } from '@/actions/user/profilePost'
 import { CardUserComponent } from '@/components/dashboard/cardUser'
 import { CardPostsProfileComponent } from '@/components/user/profile/cardPostProfile'
 
-export default async function Profile() {
-  const data = await ShowProfilePost()
+interface SearchParamsProps {
+  searchParams: {
+    page: number
+  }
+}
+
+export default async function Profile({ searchParams }: SearchParamsProps) {
+  let { page } = searchParams || 1
+  if (page === undefined) page = 1
+
+  const dt = await ShowProfilePost(page)
+  const data = dt?.data
+  const count = dt?.countPage
 
   return (
     <div className="min-h-screen text-white flex gap-4 p-4 max-md:flex-col ">
@@ -11,7 +22,7 @@ export default async function Profile() {
         <CardUserComponent />
       </div>
       <div className="space-y-4 md:w-[75%] md:max-w-full">
-        <CardPostsProfileComponent data={data} />
+        <CardPostsProfileComponent data={data} query={page} countPage={count} />
       </div>
     </div>
   )
