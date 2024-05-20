33f4ccfc2fdb884abd158887af316bb2294386ba
Andre <andrealbuquerque2001@gmail.com>
2024-05-20 16:58:24 -0300

    feat: create new action

---
+++ interface/src/actions/comments/delete.tsx
@@ -0,0 +1,21 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import { RevalidateTag } from '@/functions/revalidateTag'
+import { cookies } from 'next/headers'
+
+export async function DeleteCommentAction(idComment: string) {
+  try {
+    await ApiAction(`/comment/delete/${idComment}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  RevalidateTag('userPost')
+}
---
+++ interface/src/actions/post/delete.tsx
@@ -0,0 +1,21 @@
+'use server'
+
+import ApiAction from '@/functions/data/apiAction'
+import { RevalidateTag } from '@/functions/revalidateTag'
+import { cookies } from 'next/headers'
+
+export async function DeletePostAction(idPost: string) {
+  try {
+    await ApiAction(`/post/delete/${idPost}`, {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+        Authorization: 'Bearer ' + cookies().get('token')?.value,
+      },
+    })
+  } catch (error) {
+    console.log(error)
+  }
+
+  RevalidateTag('posts')
+}
