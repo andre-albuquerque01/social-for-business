09540fe0f8eed331f4aef22e642cb4a13830cf22
Andre <andrealbuquerque2001@gmail.com>
2024-05-21 13:09:00 -0300

    feat: update post service

--- Api/app/Services/PostService.php
+++ Api/app/Services/PostService.php
@@ -15,7 +15,13 @@ class PostService
     public function index()
     {
         try {
-            $post = Posts::with('comments')->with('rates')->whereNull("posts.deleted_at")->latest('posts.updated_at')->paginate(100);
+            $post = Posts::with(['comments' => function ($query) {
+                $query->whereNull("comments.deleted_at");
+            }])
+                ->with('rates')
+                ->whereNull("posts.deleted_at")
+                ->latest('posts.updated_at')
+                ->paginate(100);
             return PostResource::collection($post);
         } catch (Exception $th) {
             throw new PostException('Error to list post');
@@ -28,7 +34,7 @@ class PostService
             if (isset($data['imageUrlOne'])) {
                 $image = $data['imageUrlOne'];
                 if ($image->getClientOriginalExtension() != null) {
-                    $newName_image = date("H-i-s-d-m-Y.") . $image->getClientOriginalExtension();
+                    $newName_image = uniqid() . "." . $image->getClientOriginalExtension();
                     Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
                     $data['imageUrlOne'] = $newName_image;
                 } else {
@@ -49,9 +55,13 @@ class PostService
     public function showUser()
     {
         try {
-            $show = auth()->user()->posts()->with(['comments' => function ($query) {
-                $query->whereNull("comments.deleted_at");
-            }])->with('rates')->whereNull("posts.deleted_at")->paginate(100);
+            $show = auth()->user()->posts()
+                ->with(['comments' => function ($query) {
+                    $query->whereNull("comments.deleted_at");
+                }])
+                ->with('rates')
+                ->whereNull("posts.deleted_at")
+                ->paginate(100);
             return PostResource::collection($show);
         } catch (Exception $th) {
             throw new PostException('Error creating post');
@@ -61,7 +71,14 @@ class PostService
     public function showPostUser(string $idUser)
     {
         try {
-            $post = Posts::where('posts.user_idUser', '=', $idUser)->with('comments')->with('rates')->whereNull("posts.deleted_at")->orderBy('posts.updated_at', 'DESC')->paginate(100);
+            $post = Posts::where('posts.user_idUser', '=', $idUser)
+                ->with(['comments' => function ($query) {
+                    $query->whereNull("comments.deleted_at");
+                }])
+                ->with('rates')
+                ->whereNull("posts.deleted_at")
+                ->orderBy('posts.updated_at', 'DESC')
+                ->paginate(100);
             return PostResource::collection($post);
         } catch (Exception $th) {
             throw new PostException('Error creating post');
@@ -71,23 +88,24 @@ class PostService
     public function update(array $data, string $idPost)
     {
         try {
-            if (isset($data['imageUrlOne'])) {
-                $image = $data['imageUrlOne'];
-                if ($image->getClientOriginalExtension() != null) {
-                    $newName_image = date("H-i-s-d-m-Y.") . $image->getClientOriginalExtension();
-                    Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
-                    $data['imageUrlOne'] = $newName_image;
-                } 
-            } else {
-                $data['imageUrlOne'] = null;
-            }
-
             $user = auth()->user()->idUser;
-
             $post = Posts::where('idPost', $idPost)->where('user_idUser', $user)->first();
 
             if (!$post) {
                 return new GeneralResource(["message" => "Unathorized"]);
+            }
+
+            if (isset($data['imageUrlOne'])) {
+                $image = $data['imageUrlOne'];
+                if ($image->getClientOriginalExtension() != null) {
+                    $newName_image = uniqid() . "." . $image->getClientOriginalExtension();
+                    Storage::disk('public')->put('img/' . $newName_image, file_get_contents($image));
+                    $data['imageUrlOne'] = $newName_image;
+                } else {
+                    $data['imageUrlOne'] = $post->imageUrlOne;
+                }
+            } else {
+                $data['imageUrlOne'] = null;
             }
 
             $post->update($data);
