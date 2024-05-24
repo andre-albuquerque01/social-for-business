d07acc4e197fde7d084946fb685c985270ce60b6
Andre <andrealbuquerque2001@gmail.com>
2024-05-24 12:47:58 -0300

    feat: create new component

--- interface/src/components/user/profile/cardPostProfile.tsx
+++ interface/src/components/user/profile/cardPostProfile.tsx
@@ -22,7 +22,7 @@ export const CardPostsProfileComponent = async ({
 
   return (
     <>
-      {data &&
+      {data.length > 0 && data ? (
         data.map((post) => (
           <div className="bg-zinc-800 min-h-32 p-6 " key={post.idPost}>
             <div className="flex justify-between items-center">
@@ -58,12 +58,14 @@ export const CardPostsProfileComponent = async ({
                 {post.comments.map((comment) => (
                   <div key={comment.idComment} className="p-3">
                     <div className="flex justify-between items-center">
-                      <div>
-                        <span className="uppercase font-bold">
-                          {comment.firstName}
-                        </span>{' '}
-                        <span> {comment.lastName}</span>
-                      </div>
+                      {comment.idUser && (
+                        <Link href={`/user/profile/${comment.idUser}`}>
+                          <span className="uppercase font-bold">
+                            {comment.firstName}
+                          </span>{' '}
+                          <span> {comment.lastName}</span>
+                        </Link>
+                      )}
                       <div className="opacity-50 flex gap-2">
                         {FormatData(comment.created_at)}
                         {user.idUser === comment.idUser && (
@@ -82,7 +84,12 @@ export const CardPostsProfileComponent = async ({
               <CommentForm idPost={post.idPost} />
             </div>
           </div>
-        ))}
+        ))
+      ) : (
+        <p className="text-center font-bold md:mt-20">
+          Ainda não post desse usuário.
+        </p>
+      )}
       <LinkPagination query={query} countPage={countPage} />
     </>
   )
