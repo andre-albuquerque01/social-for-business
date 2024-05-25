f8b944503cd649dcac443c586bddca8f7430a197
Andre <andrealbuquerque2001@gmail.com>
2024-05-25 10:09:41 -0300

    feat: create new layout

---
+++ interface/src/app/user/(notoken)/layout.tsx
@@ -0,0 +1,16 @@
+import verifyToken from '@/functions/verify-token'
+import { Metadata } from 'next'
+import { cookies } from 'next/headers'
+import { redirect } from 'next/navigation'
+import { ReactNode } from 'react'
+
+export const metadata: Metadata = {
+  title: 'Usuário',
+}
+export default async function Layout({ children }: { children: ReactNode }) {
+  const cookiesStore = cookies()
+  const token = cookiesStore.get('token')!.value
+  const verify = await verifyToken(token)
+  if (verify) redirect('/dashboard')
+  return <div className="mx-auto h-screen max-w-[1200px]">{children}</div>
+}
