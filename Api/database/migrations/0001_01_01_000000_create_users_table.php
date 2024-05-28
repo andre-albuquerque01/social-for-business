0e1f806c9e1181749484f4ce5711f3afb4fdb383
Andre <andrealbuquerque2001@gmail.com>
2024-05-28 12:59:28 -0300

    feat: update migrate

--- Api/database/migrations/0001_01_01_000000_create_users_table.php
+++ Api/database/migrations/0001_01_01_000000_create_users_table.php
@@ -21,8 +21,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
-            $table->timestamps('deleted_at');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
