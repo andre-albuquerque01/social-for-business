d520974f134f484964fc70e469dfa56bc62bb8b4
Andre <andrealbuquerque2001@gmail.com>
2024-05-31 12:10:27 -0300

    feat: update component

--- interface/src/components/other/dropdownComment.tsx
+++ interface/src/components/other/dropdownComment.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState } from 'react'
-import { SlOptions } from 'react-icons/sl'
+import { SlOptionsVertical } from 'react-icons/sl'
 import { DeleteCommentComponent } from '../comment/deleteComment'
 
 export const DropdownComment = ({ idComment }: { idComment: string }) => {
@@ -14,9 +14,9 @@ export const DropdownComment = ({ idComment }: { idComment: string }) => {
     <div className="relative">
       <button
         onClick={toggleDropdown}
-        className="px-4 bg-gray-600 text-white rounded focus:outline-none focus:bg-gray-700"
+        className="max-md:px-2 px-1 py-1 bg-gray-600 text-white rounded focus:outline-none focus:bg-gray-700"
       >
-        <SlOptions className="w-5 h-5" />
+        <SlOptionsVertical className="max-md:w-3 w-5 h-5" />
       </button>
       {isOpen && (
         <div className="absolute right-0 mt-2 w-48 bg-zinc-600 border rounded ">
