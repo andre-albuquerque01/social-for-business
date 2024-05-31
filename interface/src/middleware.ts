2bb7fe7bb92f5a314c83d11073cc25096c5ed9bf
Andre <andrealbuquerque2001@gmail.com>
2024-05-31 12:08:50 -0300

    feat: update middleware

--- interface/src/middleware.ts
+++ interface/src/middleware.ts
@@ -4,12 +4,12 @@ import verifyToken from './functions/verify-token'
 export async function middleware(request: NextRequest) {
   const token = request.cookies.get('token')?.value
   const authentication = token ? await verifyToken(token) : false
-  if (!authentication && request.nextUrl.pathname.startsWith('/dashboard')) {
+  if (!authentication && request.nextUrl.pathname.startsWith('/')) {
     return NextResponse.redirect(new URL('/', request.url))
   }
 
-  // if (authentication && request.nextUrl.pathname.startsWith('/')) {
-  //   return NextResponse.redirect(new URL('/dashboard', request.url))
-  // }
+  if (authentication && request.nextUrl.pathname.endsWith('/')) {
+    return NextResponse.redirect(new URL('/dashboard', request.url))
+  }
   return NextResponse.next()
 }
