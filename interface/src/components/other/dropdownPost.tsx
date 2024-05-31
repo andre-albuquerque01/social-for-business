0973e6d0c3ed4b5d8c87a0157126fc8d9a0979dc
Andre <andrealbuquerque2001@gmail.com>
2024-05-31 12:11:10 -0300

    feat: update component

--- interface/src/components/other/dropdownPost.tsx
+++ interface/src/components/other/dropdownPost.tsx
@@ -1,7 +1,7 @@
 'use client'
 import Link from 'next/link'
 import React, { useState } from 'react'
-import { SlOptions } from 'react-icons/sl'
+import { SlOptionsVertical } from 'react-icons/sl'
 import { DeletePostComponent } from '../post/deletePost'
 
 export const DropdownPost = ({ idPost }: { idPost: string }) => {
@@ -15,9 +15,9 @@ export const DropdownPost = ({ idPost }: { idPost: string }) => {
     <div className="relative">
       <button
         onClick={toggleDropdown}
-        className="px-4 bg-gray-600 text-white rounded focus:outline-none focus:bg-gray-700"
+        className="max-md:px-2 px-1 py-1 bg-gray-600 text-white rounded focus:outline-none focus:bg-gray-700"
       >
-        <SlOptions className="w-5 h-5" />
+        <SlOptionsVertical className="max-md:w-3 w-5 h-5" />
       </button>
       {isOpen && (
         <div className="absolute right-0 mt-2 w-48 bg-zinc-600 border rounded ">
