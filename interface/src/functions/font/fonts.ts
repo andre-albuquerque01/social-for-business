927a90a45da4859fc43985c52872c88868e33257
Andre <andrealbuquerque2001@gmail.com>
2024-06-04 11:35:11 -0300

    feat: delete files

--- interface/src/functions/font/fonts.ts
+++
@@ -1,9 +0,0 @@
-import { Roboto } from 'next/font/google'
-
-export const fontBody = Roboto({
-  weight: ['100', '300', '400', '500', '700'],
-  style: ['normal'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto',
-})
